debf3c184a344cbc276ad41360a2fa053af0ddc8
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2025-01-06 11:08:32 +0700

    add i18n to project

---
+++ src/i18n.ts
@@ -0,0 +1,24 @@
+// src/i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from './locales/en.json';
+import id from './locales/id.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      id: { translation: id },
+    },
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 // src/pages/Home.tsx
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
   const [count, setCount] = useState(0);
+  const { t } = useTranslation();
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Home Page</h1>
+      <h1 className="text-3xl font-bold underline">{t('welcome')}</h1>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
