84fad74b8b4ff47d12c48c8e5ac72fe0e186b947
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2025-01-08 05:52:54 +0700

    add implement zustand

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 // src/pages/Home.tsx
-import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
+import { Button } from '@headlessui/react';
+import useStore from '@/store/useStore';
 
 const Home = () => {
-  const [count, setCount] = useState(0);
+  const { count, increase, decrease } = useStore();
   const { t } = useTranslation();
 
   return (
@@ -13,10 +14,24 @@ const Home = () => {
         <title>Title app</title>
         <meta name="description" content="description app" />
       </Helmet>
-      <h1 className="text-3xl font-bold underline">{t('welcome')}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <div className="p-2">
+        <h1 className="mb-2 text-3xl font-bold underline">{t('welcome')}</h1>
+        <h2>Count: {count}</h2>
+        <div className="flex flex-row gap-2">
+          <Button
+            className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-[active]:bg-sky-700 data-[hover]:bg-sky-500"
+            onClick={increase}
+          >
+            Increase
+          </Button>
+          <Button
+            className="rounded bg-red-600 px-4 py-2 text-sm text-white data-[active]:bg-red-700 data-[hover]:bg-red-500"
+            onClick={decrease}
+          >
+            Decrease
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
