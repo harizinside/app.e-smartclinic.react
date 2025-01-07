7715ef4ce4165303bb3981aed1133ad82361e5a1
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2025-01-08 05:35:56 +0700

    change react-helmet to react-helmet-async with fixed react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import AppRoutes from './routes';
 import './index.css';
 import './i18n';
 
+// Create a query client instance
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRoutes />
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>,
 );
